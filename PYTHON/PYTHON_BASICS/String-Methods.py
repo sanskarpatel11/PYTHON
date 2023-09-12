35af862a7871f07b9fd9b6929214848019baab36
sanskarpatel11 <144349107+sanskarpatel11@users.noreply.github.com>
2023-09-12 05:47:30 -0700

    Add files via upload

---
+++ PYTHON/PYTHON_BASICS/String-Methods.py
@@ -0,0 +1,23 @@
+a = "Rohit is a Good Boy!"
+
+print(len(a)) #Lenght of String
+
+print(len("I Am Just Doing My Work"))
+
+
+print(a.upper())#The upper() method converts a string to upper case.
+
+print(a.lower())#The lower() method converts a string to lower case.
+
+
+print(a.capitalize())#This Convert First Letter To Capital
+
+print(a.rstrip("!"))# Remove Any Character
+
+print(a.replace("Good","Bad"))
+
+print(a.split())#The split() method splits the given string at the specified instance and returns the separated strings as list items.
+
+print(a.endswith("!")) #The endswith() method checks if the string ends with a given value. If yes then return True, else return False.
+
+print(a.endswith("x"))
