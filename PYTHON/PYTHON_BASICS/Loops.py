521ebc5f5685273287e818ee303fbffc258d6e4b
sanskarpatel11 <144349107+sanskarpatel11@users.noreply.github.com>
2023-09-16 03:49:29 -0700

    Add files via upload

---
+++ PYTHON/PYTHON_BASICS/Loops.py
@@ -0,0 +1,15 @@
+#returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), 
+#and stops before a specified number.
+
+a = 21
+for i in range(1,11):
+  print(a," x",i, "=",a*i)
+print()
+
+ #The while loop is used to repeat a section of code an unknown number of times until a specific
+ #condition is met 
+i = 1
+b = 12
+while (i<11):
+    print(b,"x",i,"=",i*b)
+    i = i + 1
